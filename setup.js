31837d5bda5eeedd48a1552e5adfd3e8f441629d
Lars Schmidt <l-c.schmidt@folkwang-uni.de>
2026-07-05 17:58:22 +0200

    Updated setup.js for working version

--- setup.js
+++ setup.js
@@ -1,6 +1,7 @@
 await loadScript(
-  "https://cdn.statically.io/gh/LrsChrSch/HydraTools@main/imports.js?dev=1",
+  "https://cdn.statically.io/gh/LrsChrSch/HydraTools@main/imports.js?dev=10",
 );
+await hydraToolsReady;
 
 a.show();
 a.setBins(4);
@@ -11,7 +12,3 @@ a.meyda.start();
 update = (dt) => {
   updateAA();
 };
-
-osc()
-  .color(() => aa.spectralCentroid, 0, 0)
-  .out();
