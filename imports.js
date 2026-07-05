0bc3b12a65fbc8bb963c00f4fc11d717db06707a
Lars Schmidt <l-c.schmidt@folkwang-uni.de>
2026-07-05 17:46:13 +0200

    Fix: Hopefully fixing the one-line loader for everything

--- imports.js
+++ imports.js
@@ -1,35 +1,39 @@
-await loadScript(
-  "https://cdn.jsdelivr.net/gh/geikha/hyper-hydra@latest/hydra-arithmetics.js",
-);
-await loadScript(
-  "https://cdn.jsdelivr.net/gh/geikha/hyper-hydra@latest/hydra-arrays.js",
-);
-await loadScript(
-  "https://cdn.jsdelivr.net/gh/geikha/hyper-hydra@latest/hydra-blend.js",
-);
-await loadScript(
-  "https://cdn.jsdelivr.net/gh/geikha/hyper-hydra@latest/hydra-gradientmap.js",
-);
-await loadScript(
-  "https://cdn.jsdelivr.net/gh/geikha/hyper-hydra@latest/hydra-outputs.js",
-);
-await loadScript(
-  "https://cdn.jsdelivr.net/gh/geikha/hyper-hydra@latest/hydra-tap.js",
-);
-await loadScript(
-  "https://cdn.jsdelivr.net/gh/geikha/hyper-hydra@latest/hydra-text.js",
-);
-await loadScript("https://metagrowing.org/extra-shaders-for-hydra/all.js");
+async function importAll() {
+  await loadScript(
+    "https://cdn.jsdelivr.net/gh/geikha/hyper-hydra@latest/hydra-arithmetics.js",
+  );
+  await loadScript(
+    "https://cdn.jsdelivr.net/gh/geikha/hyper-hydra@latest/hydra-arrays.js",
+  );
+  await loadScript(
+    "https://cdn.jsdelivr.net/gh/geikha/hyper-hydra@latest/hydra-blend.js",
+  );
+  await loadScript(
+    "https://cdn.jsdelivr.net/gh/geikha/hyper-hydra@latest/hydra-gradientmap.js",
+  );
+  await loadScript(
+    "https://cdn.jsdelivr.net/gh/geikha/hyper-hydra@latest/hydra-outputs.js",
+  );
+  await loadScript(
+    "https://cdn.jsdelivr.net/gh/geikha/hyper-hydra@latest/hydra-tap.js",
+  );
+  await loadScript(
+    "https://cdn.jsdelivr.net/gh/geikha/hyper-hydra@latest/hydra-text.js",
+  );
+  await loadScript("https://metagrowing.org/extra-shaders-for-hydra/all.js");
 
-await loadScript(
-  "https://cdn.statically.io/gh/LrsChrSch/HydraTools@main/common.js?dev=1",
-);
-await loadScript(
-  "https://cdn.statically.io/gh/LrsChrSch/HydraTools@main/audio.js?dev=1",
-);
-await loadScript(
-  "https://cdn.statically.io/gh/LrsChrSch/HydraTools@main/shaders.js?dev=1",
-);
-await loadScript(
-  "https://cdn.statically.io/gh/LrsChrSch/HydraTools@main/colors.js?dev=1",
-);
+  await loadScript(
+    "https://cdn.statically.io/gh/LrsChrSch/HydraTools@main/common.js?dev=1",
+  );
+  await loadScript(
+    "https://cdn.statically.io/gh/LrsChrSch/HydraTools@main/audio.js?dev=1",
+  );
+  await loadScript(
+    "https://cdn.statically.io/gh/LrsChrSch/HydraTools@main/shaders.js?dev=1",
+  );
+  await loadScript(
+    "https://cdn.statically.io/gh/LrsChrSch/HydraTools@main/colors.js?dev=1",
+  );
+}
+
+await importAll();
